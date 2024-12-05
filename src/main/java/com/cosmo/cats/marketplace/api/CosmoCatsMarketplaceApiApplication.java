29f3afc1548378c177a92e0b90ae778523275dc8
ViktorMaster <viktor.master7@gmail.com>
2024-12-05 23:43:21 +0200

    Initial commit

---
+++ src/main/java/com/cosmo/cats/marketplace/api/CosmoCatsMarketplaceApiApplication.java
@@ -0,0 +1,13 @@
+package com.cosmo.cats.marketplace.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmoCatsMarketplaceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosmoCatsMarketplaceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cosmo/cats/marketplace/api/CosmoCatsMarketplaceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cosmo.cats.marketplace.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmoCatsMarketplaceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
