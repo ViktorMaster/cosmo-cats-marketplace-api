7d207c8693e822ce6b6607bab163eb4de18218d9
ViktorMaster <viktor.master7@gmail.com>
2024-12-06 00:19:48 +0200

    Add empty test classes for proper workflow

---
+++ src/test/java/com/cosmo/cats/marketplace/api/service/ProductServiceTest.java
@@ -0,0 +1,10 @@
+package com.cosmo.cats.marketplace.api.service;
+
+import org.junit.jupiter.api.Test;
+
+public class ProductServiceTest {
+    @Test
+    void test() {
+
+    }
+}
---
+++ src/test/java/com/cosmo/cats/marketplace/api/web/ProductControllerIT.java
@@ -0,0 +1,10 @@
+package com.cosmo.cats.marketplace.api.web;
+
+import org.junit.jupiter.api.Test;
+
+public class ProductControllerIT {
+    @Test
+    void test() {
+
+    }
+}
