378f4c8ffa498da4049ba16a3e24b15fde6ffa02
alionafomenko <a.fomenko2005@gmail.com>
2024-12-06 02:03:46 +0200

    Add custom validation

--- src/main/java/com/cosmo/cats/marketplace/api/web/dto/product/ProductCreationDto.java
+++ src/main/java/com/cosmo/cats/marketplace/api/web/dto/product/ProductCreationDto.java
@@ -1,5 +1,7 @@
 package com.cosmo.cats.marketplace.api.web.dto.product;
 
+import com.cosmo.cats.marketplace.api.web.dto.validation.CosmicWordCheck;
+import com.cosmo.cats.marketplace.api.web.dto.validation.ExtendedValidation;
 import jakarta.validation.GroupSequence;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
@@ -10,9 +12,11 @@ import lombok.Value;
 
 @Value
 @Builder(toBuilder = true)
+@GroupSequence({ProductCreationDto.class, ExtendedValidation.class})
 public class ProductCreationDto {
     @NotNull(message = "Name cannot be null")
     @Size(min = 3, max = 50, message = "Name must be between 3 and 100 characters")
+    @CosmicWordCheck(groups = ExtendedValidation.class)
     String name;
     @Size(max = 255, message = "Description cannot exceed 255 characters")
     @NotNull(message = "description cannot be null")
