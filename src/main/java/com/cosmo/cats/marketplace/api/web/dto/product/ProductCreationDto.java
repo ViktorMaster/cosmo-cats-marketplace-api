330d57870ef6c3d5ea27d17395bc4a650dcf9c9f
alionafomenko <a.fomenko2005@gmail.com>
2024-12-06 02:00:22 +0200

    Add product dto for web layer

---
+++ src/main/java/com/cosmo/cats/marketplace/api/web/dto/product/ProductCreationDto.java
@@ -0,0 +1,24 @@
+package com.cosmo.cats.marketplace.api.web.dto.product;
+
+import jakarta.validation.GroupSequence;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Value;
+
+@Value
+@Builder(toBuilder = true)
+public class ProductCreationDto {
+    @NotNull(message = "Name cannot be null")
+    @Size(min = 3, max = 50, message = "Name must be between 3 and 100 characters")
+    String name;
+    @Size(max = 255, message = "Description cannot exceed 255 characters")
+    @NotNull(message = "description cannot be null")
+    @NotBlank(message = "description cannot be blank")
+    String description;
+    @NotNull(message = "Price cannot be null")
+    @DecimalMin(value = "0.0", inclusive = false)
+    double price;
+}
